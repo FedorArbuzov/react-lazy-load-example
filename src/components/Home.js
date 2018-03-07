234e2078a3191dd3ceda9e3114de8c348bc80d8c
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-03-07 11:01:43 +0300

    add travis ci functional

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Home() {
-    return <h1>Home</h1>;
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {email: '', password: ''};
+
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleEmail(event) {
+        this.setState({email: event.target.value});
+    }
+
+    handlePassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert(this.state.email + ' ' + this.state.password);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Email:
+                    <input type="email" value={this.state.value} onChange={this.handleEmail} />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" value={this.state.value} onChange={this.handlePassword} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
 }
